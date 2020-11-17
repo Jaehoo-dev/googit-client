9563d73093e312c741d4affa28de4cd670c3d4c5
KE LI <like19911209@hotmail.com>
2020-11-18 04:09:08 +0900

    refactor: modal toggle react state changes to redux state

--- src/components/AppMain/NoteList/index.js
+++ src/components/AppMain/NoteList/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import fetchNoteList from '../../../api/noteListRequest';
+import fetchNoteList from '../../../api/noteListFetch';
 import NoteListHead from './NoteListHead';
 import NoteListBody from './NoteListBody';
 
@@ -9,13 +9,18 @@ const Wrapper = styled.div`
 `;
 
 export default function NoteList({ isPrivate, currentUser }) {
-  let notes;
+  let notes
 
-  useEffect(() => {
-    console.log(isPrivate, 'note list');
-    const result = fetchNoteList(isPrivate, currentUser);
-    // notes = result.xxx
-  });
+
+  // useEffect(() => {
+  //   // console.log(isPrivate, 'note list');
+  //   async function getNoteList() {
+  //   const noteList = await fetchNoteList(isPrivate, currentUser);
+  //   console.log(noteList, '이자리로 디스패치 함수를 불러서 리덕스에 넣는다.') // 이 유즈 이펙트 앱으로 올라가야할듯 
+  //   }
+  //   getNoteList();
+    
+  // }, [isPrivate]);
 
   const tempNote = [1, 'title1', 'han', 20201116, 'shared'];
 
