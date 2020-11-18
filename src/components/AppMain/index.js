eae85551448a8e45bdba4fb0e42f1f6ec0329ac6
KE LI <like19911209@hotmail.com>
2020-11-18 15:13:11 +0900

    feat: keyword searched note list to redux

--- src/components/AppMain/index.js
+++ src/components/AppMain/index.js
@@ -3,11 +3,17 @@ import { Route, Switch } from 'react-router-dom';
 import AppHeader from '../AppHeader';
 import NoteList from './NoteList';
 
-export default function AppMain({ onLogout, isPrivate, handleOnClick, currentUser }) {
+export default function AppMain({ onLogout, isPrivate, handleOnClick, currentUser, onLoad }) {
 
   return (
     <>
-      <AppHeader isPrivate={isPrivate} handleOnClick={handleOnClick} onLogout={onLogout} />
+      <AppHeader
+        isPrivate={isPrivate}
+        handleOnClick={handleOnClick}
+        onLogout={onLogout}
+        onLoad={onLoad}
+        currentUser={currentUser}
+      />
       <Switch>
         <Route path='/profile/edit'>
           <div>edit</div>
