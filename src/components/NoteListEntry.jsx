6dc83a2ca0eaffab9acc90a9cfcb6ae39db596c0
Jaehoo Im <im_jaehoo@kakao.com>
2020-11-29 21:23:18 +0900

    refactor: create dateFormatter util function and reuse it

--- src/components/NoteListEntry.jsx
+++ src/components/NoteListEntry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { NoteListEntryWrapper } from '../styledComponents/MainBody.styled';
+import dateFormatter from '../utils/dateFormatter';
 
 export default function NoteListEntry({
   entryInfos,
@@ -11,8 +12,8 @@ export default function NoteListEntry({
   const isShared = !!entryInfos.branch.sharing_infos.length;
   const title = entryInfos.latestNote.blocks[0].children[0].text;
   const updatedAt = entryInfos.branch.updated_at;
-  const date = updatedAt.substring(0, 10);
-  const time = updatedAt.substring(11, 16);
+  // const date = updatedAt.substring(0, 10);
+  // const time = updatedAt.substring(11, 16);
 
   function NoteListEntryClickHandler(note, branch) {
     onNoteListEntryClick(note, branch);
@@ -33,7 +34,7 @@ export default function NoteListEntry({
         <div data-testid='count'>{count + 1}</div>
         <div data-testid='title'>{title}</div>
         <div data-testid='creator'>{creator}</div>
-        <div data-testid='updatedAt'>{`${date} ${time}`}</div>
+        <div data-testid='updatedAt'>{`${dateFormatter(updatedAt)}`}</div>
         <div data-testid='isShared'>{isShared ? 'O' : 'X'}</div>
       </NoteListEntryWrapper>
     </Link>
